eec5122c085dd12842fe8e5dc6af91dfdd9a066c
Raihanudin Rafif <raihanudinrafif@gmail.com>
2023-05-09 14:07:16 +0700

    latest

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,8 @@
 import '@/styles/globals.css'
-import SiswaInputProvider from '@/component/GlobalState/SiswaInputProvider'
-import DisplaySiswaProvider from '@/component/GlobalState/DisplaySiswaProvider'
 
 export default function App({ Component, pageProps }) {
   return (
-    <SiswaInputProvider>
-      <DisplaySiswaProvider>
         <Component {...pageProps} />
-      </DisplaySiswaProvider>
-    </SiswaInputProvider>
   )
 }
 
